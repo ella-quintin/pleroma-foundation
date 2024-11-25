afdc864439816d58be03d1bca092298e61226262
ella-quintin <equintin41@gmail.com>
2024-11-25 14:11:35 +0000

    Third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Landing from './pages/landing';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Landing />,
+    },
+  ])
 
 
   return (
     <>
-      
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ src/pages/hwd/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HowWeDo = () => {
+  return (
+    <div>HowWeDo</div>
+  )
+}
+
+export default HowWeDo
